829850f28958b75f1580e5fa2f39277ff5d12170
Sabet <mohammad.ehsani74@gmail.com>
2017-11-15 21:33:37 +0330

    Add client script

---
+++ eventsExtractor.js
@@ -0,0 +1,32 @@
+let listOfEvents = [] ;
+let elements = [];
+function eventsExtractor(){
+	let eachEvent = {};
+	elements = document.querySelectorAll('#commentary li');
+	let num ;
+	for(num = 1 ; num < elements.length ; num++){
+		eachEvent = { relativeTime : elements[num].innerHTML.split('time">')[1].split(' </div')[0],
+		eventComment : elements[num].innerHTML.split('comment">')[1].split('<br>')[0]};
+		listOfEvents.push(eachEvent);
+	}
+	sendData(listOfEvents);
+}
+
+function sendData(eventsList){
+	var xhr = new XMLHttpRequest();
+	var url = "http://localhost:3000/events/sendData";
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json");
+	xhr.onreadystatechange = function () {
+	    if (xhr.readyState === 4 && xhr.status === 200) {
+	        var response = xhr.responseText;
+	        if(response === true){
+	        	console.log('- ' + list.length + ' events extracted successfully');
+	        }
+	    }
+	};
+	xhr.send(JSON.stringify({eventsList:eventsList}));
+}
+
+
+eventsExtractor();
