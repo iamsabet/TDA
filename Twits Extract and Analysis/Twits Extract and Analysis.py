0ab8487dbed18e6a16405f443d60281a0907969f
iamsabet <mohammad.ehsani74@gmail.com>
2017-11-20 11:31:16 +0330

    lvl 1 removing urls - emojies and emoticons from mocked datas

--- Twits Extract and Analysis/Twits Extract and Analysis.py
+++ Twits Extract and Analysis/Twits Extract and Analysis.py
@@ -1,12 +1,38 @@
 from flask import Flask
-
+import re
+import remove_emoji
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Twits Extract and Analysis'
 
+@app.route('/cleaner')
+def cleaner():
+    # query to get specific list of twits ...
+    # list of strings we play here ... :
+    # mockedData ...
+    print('Remove Stop Words , Emojies , Emoticons , Url')
+    twitText = "hello i have :'((( :(((( just seen it shiiiittt :) https://example.com/some"
+    print(twitText)  # with emojies
+    removedEmojiesText  = remove_emoji.remove_emoji(twitText)
+    print(removedEmojiesText) # removed emojies
+    wordsArray = removedEmojiesText.split(' ');
+    removedURLs = [];
+    y = 0 ;
+    for x in range (0,len(wordsArray)): # remove emoticons and urls both
+        removedUrlText = re.sub(r':.*$', "", wordsArray[x])
+        print(removedURLsgi)
+        if removedUrlText!="":
+            removedURLs.append(wordsArray[x]);
+            y += 1;
+
+    print(removedURLs); # remove urls
+
+    print(len(removedURLs))  # removed emojies -- emoticons and smileis like :) - :( mus be removed too
+
+    return 'Twits Extract and Analysis'
 
 if __name__ == '__main__':
     app.run()
