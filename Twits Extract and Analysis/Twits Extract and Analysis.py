cb94405395ed23b6ac1341b45f3b332ace5d98d9
iamsabet <mohammad.ehsani74@gmail.com>
2017-11-21 16:58:12 +0330

    phase 2 done ! stemmer needs debug

--- Twits Extract and Analysis/Twits Extract and Analysis.py
+++ Twits Extract and Analysis/Twits Extract and Analysis.py
@@ -5,14 +5,21 @@ from nltk.tokenize import word_tokenize
 import re
 import remove_emoji
 from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
+from nltk.stem import WordNetLemmatizer
+
+import pymysql
 app = Flask(__name__)
 
 
 userpass = 'mysql+pymysql://root:@'
-basedir  = '192.168.43.138'
+basedir  = '192.168.53.236'
 dbname   = '/soccer'
 socket   = '?unix_socket=/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock'
 db = SQLAlchemy(app)
+
+
+engine = sqlalchemy.create_engine('mysql+pymysql://root:147852@localhost/soccer')
 
 @app.route('/')
 def hello_world():
@@ -21,37 +28,40 @@ def hello_world():
 
 @app.route('/cleaner')
 def cleaner():
-    if db.session.query('1').from_statement('1').all():
-        print(db.session.query('*').from_statement('eventText').all())
-        return 'It works.'
-    else:
-        return 'Something is broken.'
+    lem = WordNetLemmatizer()
+    result = engine.execute('select eventText FROM soccerevents')
+    if result:
+        for x in result:
+            print('Remove Stop Words , Emojies , Emoticons , Urls ... and tokenize')
+            twitText = x[0]
+            print(twitText)  # with     emojies
+            removedEmojiesText = remove_emoji.remove_emoji(twitText)
+            print(removedEmojiesText)
+            word_tokens = word_tokenize(removedEmojiesText)
+            print(word_tokens)
 
-    # query to get specific list of twits ...
-    # list of strings we play here ... :
-    # mockedData ...
-    print('Remove Stop Words , Emojies , Emoticons , Urls ... and tokenize')
-    twitText = "Whadddddupppp United fans. Do you remember http://web.ali.com that time when you lost 4-0 at the Bridge and you came back a year later and were lucky enough to only lose 1-0? "
-    print(twitText)  # with     emojies
-    removedEmojiesText = remove_emoji.remove_emoji(twitText)
-    word_tokens = word_tokenize(removedEmojiesText)
-    print(word_tokens)
+            stop_words = set(stopwords.words('english'))
+            print(stop_words)
+            filtered_sentence = []
+            for w in word_tokens:
+                if w not in stop_words:
+                    if len(w) > 1 and not w.startswith('/'):
+                        removedUrlText = re.sub(r'http.*$', "", w);
+                        if(removedUrlText != ''):
+                            filtered_sentence.append(removedUrlText)
 
-    stop_words = set(stopwords.words('english'))
-    print(stop_words)
-    filtered_sentence = [w for w in word_tokens if not w in stop_words]
-    filtered_sentence = []
-    for w in word_tokens:
-        if w not in stop_words:
-            if len(w) > 1 and not w.startswith('/'):
-                removedUrlText = re.sub(r'http.*$', "", w);
-                if(removedUrlText != ''):
-                    filtered_sentence.append(removedUrlText)
+            print('Word tokens : ')
+            print(word_tokens)
+            print('filtered Scentence : ')
 
-    print('Word tokens : ')
-    print(word_tokens)
-    print('filtered Scentence : ')
-    print(filtered_sentence)
+            stempedlist = []
+            for i in word_tokens:
+                stemword=lem.lemmatize(i)
+                stempedlist.append(stemword)
+
+            print(stempedlist)
+
+
 
     return jsonify(filtered_sentence)
 
