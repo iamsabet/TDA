e5acb6e5da0237a519175585b1509c894062c442
iamsabet <mohammad.ehsani74@gmail.com>
2017-12-14 15:08:59 +0330

    Translate Twits - Final Only

--- Twits Extract and Analysis/Twits Manipulator.py
+++ Twits Extract and Analysis/Twits Manipulator.py
@@ -1,7 +1,6 @@
 from pymongo import MongoClient
 import datetime
 from nltk.stem.wordnet import WordNetLemmatizer
-from nltk.classify import NaiveBayesClassifier
 from googletrans import Translator
 lmtzr = WordNetLemmatizer()
 epoch = datetime.datetime.utcfromtimestamp(0)
@@ -11,16 +10,40 @@ db = client.twitsDb
 twits = db.twits
 events = db.events
 
-tweets = twits.find({"twitHashtags": {"$in": ["1-0","portugal", "cr7", "cristianoronaldo", "ronaldo", "pepe", "por", "france", "fra",
+tweets = twits.find({"twitHashtags": {"$in": ["1-0","goal","gol","goaall","goals","gool","captain","referre","penalty","foul","portugal", "cr7", "cristianoronaldo", "ronaldo", "pepe", "por", "france", "fra",
             "euro2016", "final", "amp", "supporting", "euro" , "support"]},
-            "twitmiliSeconds": {"$gt": 1468108920000, "$lt": 1468117860000}}, {"twitTokens": 1, "twitmiliSeconds": 1, "positiveFeelings" : 1, "NegativeFeelings": 1, "twitId" : 1})
-print(tweets)
-x = 0
+            "twitmiliSeconds": {"$gt": 1468108920000, "$lt": 1468117860000}}, {"twitTokens": 1, "twitmiliSeconds": 1, "positiveFeelings" : 1, "NegativeFeelings": 1, "twitId" : 1,"twitText": 1,"tokensFeeling":1})
+
+successfull = 0
+failed = 0
 translator = Translator()
 for tweet in tweets:
-        translateds = translator.translate(tweet["twitTokens"], dest='en')
-        for translated in translateds:
-            print(translated.text)
-            x += 1
+    translatedTokens = []
+    try:
+        twitText = tweet["twitText"]
+        translatedTwitText = translator.translate(tweet["twitText"], dest='en')
+
+        for twitToken in tweet["twitTokens"]:
+            translated = translator.translate(twitToken, dest='en')
+            translatedTokens.append(translated.text)
+
+        twits.update_one(
+            {"twitId": tweet["twitId"]},
+                {
+                "$set": {
+                    "twitTokens": translatedTokens,
+                    "twitText": translatedTwitText.text
+                }
+            }
+        )
+        successfull += 1
+        print("successFulls : ", successfull)
+    except:
+        failed += 1
+        print("faileds : ", failed)
+
+print("Finished \n")
+print("successFulls : ", successfull)
+print("faileds : " , failed)
 
 # Predict
--- Twits Extract and Analysis/plot.py
+++ Twits Extract and Analysis/plot.py
@@ -1,22 +1,11 @@
-from flask import json
-from pymongo import MongoClient
-import datetime
-from nltk.stem.wordnet import WordNetLemmatizer
-lmtzr = WordNetLemmatizer()
-epoch = datetime.datetime.utcfromtimestamp(0)
-
-from flask import Flask
-app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    client = MongoClient('mongodb://localhost:27017')
-    db = client.twitsDb
-    twits = db.twits
-    events = db.events
-    tweets = twits.find({"twitmiliSeconds": {"$gt": 1468108920000, "$lt": 1468118160000}})  # 5 mins after the game
 
+
+
+
+tweets = twits.find(
+        {"twitmiliSeconds": {"$gt": 1468108920000, "$lt": 1468118160000}})  # 5 mins after the game
     lastStep = 1468108920000
     minutes = 0
     lastMode = 0
@@ -44,17 +33,12 @@ def hello_world():
         eventList = events.find({"relativeTime": x})
         for event in eventList:
             if event["relativeTime"]:
-                eventTimeLine.append({"eventTime": x, "name": event["eventName"],"eventKey": event["eventKey"], "subject": event["subject"]})
+                eventTimeLine.append({"eventTime": x, "name": event["eventName"], "subject": event["subject"]})
 
     print(twitNumbers)
     print(eventTimeLine)
     #   Range of scales between freezing to boiling water
     return json.jsonify({"axis": axis, "twitNumbers": twitNumbers, "eventTimeLines": eventTimeLine})
-
-
-if __name__ == '__main__':
-    app.run()
-
 
 
 
